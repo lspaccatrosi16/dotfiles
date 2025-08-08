7d587aff499281991465e2b283599e3fcc368ec5
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2025-08-09 00:59:06 +0100

    flesh out bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 printf "Package manager: " 
 read pm
 
-pkgs=(git neovim fastfetch ripgrep fzf tmux zsh hyprland rofi-wayland quickshell greetd tuigreet hyprpaper cliphist alacritty)
+pkgs=(git neovim fastfetch ripgrep fzf tmux zsh hyprland hyprland-devel rofi-wayland quickshell greetd tuigreet hyprpaper cliphist alacritty nautilus fira-code-fonts btop gnome-keyring acpi pipewire pavucontrol)
 
 case "$pm" in
   zypper) 
@@ -19,10 +19,19 @@ case "$pm" in
       ;;
   dnf)    
       echo "Installing packages"
+      sudo dnf copr -y enable errornointernet/quickshell
+      sudo dnf copr -y enable alternateved/cliphist
+      sudo dnf copr -y enable solopasha/hyprland
       sudo dnf install -y "${pkgs[@]}"
       ;;
   *)
       echo "Package manager $pm not supported"
+      exit 1
       ;;
     
 esac
+
+sudo systemctl disable display-manager.service
+sudo systemctl enable greetd.service
+sudo useradd -M -G video greeter
+sudo cp ./greetd.toml /etc/greetd/conf.toml
